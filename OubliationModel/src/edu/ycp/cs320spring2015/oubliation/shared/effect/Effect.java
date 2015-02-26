0885f1fabb744da09aa9a0114ffc809f6c10936c
Kevin Grasso <kevgrasso@gmail.com>
2015-02-25 19:21:35 -0500

    fixed error where effects could not be Void

--- OubliationModel/src/edu/ycp/cs320spring2015/oubliation/shared/effect/Effect.java
+++ OubliationModel/src/edu/ycp/cs320spring2015/oubliation/shared/effect/Effect.java
@@ -2,6 +2,6 @@ package edu.ycp.cs320spring2015.oubliation.shared.effect;
 
 import edu.ycp.cs320spring2015.oubliation.shared.character.Unit;
 
-public interface Effect<T extends Unit> {
+public interface Effect<T> {
 	public void apply(T target);
 }
