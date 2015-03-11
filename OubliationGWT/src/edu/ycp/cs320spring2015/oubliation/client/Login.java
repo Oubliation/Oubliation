9d3b1f23a52352f77130314ab46f8ac843b002e2
Kevin Grasso <kgrasso3@ycp.edu>
2015-03-11 17:48:56 -0400

    Login fixed

---
+++ OubliationGWT/src/edu/ycp/cs320spring2015/oubliation/client/Login.java
@@ -0,0 +1,19 @@
+package edu.ycp.cs320spring2015.oubliation.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class Login extends Composite {
+
+	private static LoginUiBinder uiBinder = GWT.create(LoginUiBinder.class);
+
+	interface LoginUiBinder extends UiBinder<Widget, Login> {
+	}
+
+	public Login() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
