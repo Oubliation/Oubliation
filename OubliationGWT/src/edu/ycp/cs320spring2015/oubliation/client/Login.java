67706008e0146716f8117af57871cdb633fb6833
Kevin Grasso <kevgrasso@gmail.com>
2015-03-12 15:06:35 -0400

    Added beginning of login server communication.

--- OubliationGWT/src/edu/ycp/cs320spring2015/oubliation/client/Login.java
+++ OubliationGWT/src/edu/ycp/cs320spring2015/oubliation/client/Login.java
@@ -1,8 +1,14 @@
 package edu.ycp.cs320spring2015.oubliation.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Login extends Composite {
@@ -11,9 +17,21 @@ public class Login extends Composite {
 
 	interface LoginUiBinder extends UiBinder<Widget, Login> {
 	}
+	
+	@UiField TextBox username;
+	@UiField PasswordTextBox password;
+	
 
 	public Login() {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
+
+	@UiHandler("loginButton")
+	void onClick(ClickEvent e) {
+		if (DataKeeper.isProfile(username.getText(), password.getText())) {
+			
+		}
 	}
 
 }
