b07432337341de33a79aafc4e12e9b46b7b16e5f
Brandon Miller <wildfire4@comcast.net>
2015-04-06 14:50:12 -0400

    Worked on ViewDungeon and the Dungeon Model classes part 1.

--- OubliationGWT/src/edu/ycp/cs320spring2015/oubliation/client/ViewDungeon.java
+++ OubliationGWT/src/edu/ycp/cs320spring2015/oubliation/client/ViewDungeon.java
@@ -3,13 +3,17 @@ package edu.ycp.cs320spring2015.oubliation.client;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.ycp.cs320spring2015.oubliation.shared.Profile;
 import edu.ycp.cs320spring2015.oubliation.shared.location.Dungeon;
+import edu.ycp.cs320spring2015.oubliation.shared.location.Ordinal;
 
 /**
  * 
@@ -24,6 +28,8 @@ public class ViewDungeon extends Composite {
 	interface ViewDungeonUiBinder extends UiBinder<Widget, ViewDungeon> {
 	}
 	
+	@UiField Label dungeonLvl;
+	
 	@UiField(provided = true) Canvas canvas; //canvas HTML object
 	private final Context2d context; //Canvas 2d drawing context
 	private final Profile profile;
@@ -35,13 +41,34 @@ public class ViewDungeon extends Composite {
 		
 		this.profile = profile;
 		dungeon = new Dungeon(0);
-		context = canvas.getContext2d(); 
+		context = canvas.getContext2d();
+		dungeonLvl.setText("You are on dungeon level " + Integer.toString(dungeon.getLevel()));
 	}
+	
+	@UiHandler("Forward")
+	void onClickForward() {
+		dungeon.move(Ordinal.forward, profile);
+	}
+	
+	@UiHandler("Backward")
+	void onClickBackward() {
+		dungeon.move(Ordinal.backward, profile);
+	}
+	
+	@UiHandler("Left")
+	void onClickLeft() {
+		dungeon.move(Ordinal.left, profile);
+	}
+	
+	@UiHandler("Right")
+	void onClickRight() {
+		dungeon.move(Ordinal.right, profile);
+	}
+	
 	
 	/**
 	 * renders current dungeon view to context
 	 */
 	private void renderDungeon() {
-		
 	}
 }
--- OubliationModel/src/edu/ycp/cs320spring2015/oubliation/shared/location/Dungeon.java
+++ OubliationModel/src/edu/ycp/cs320spring2015/oubliation/shared/location/Dungeon.java
@@ -4,9 +4,11 @@ import edu.ycp.cs320spring2015.oubliation.shared.Profile;
 
 public class Dungeon {
 	
-	private int x;
-	private int y;
+	private int playerX;
+	private int playerY;
 	private Cardinal facing;
+	private Tile [][]map;
+	private int level;
 	
 	public Dungeon(int floor) {
 	}
@@ -15,10 +17,46 @@ public class Dungeon {
 		switch(direction) {
 		case left: 
 			facing = facing.rotateLeft();
+			break;
+		case right: 
+			facing = facing.rotateRight();
+			break;
+		case forward: 
+			playerX += facing.getX();
+			playerY += facing.getY();
+			break;
+		case backward:
+			playerX -= facing.getX();
+			playerY -= facing.getY();
+			break;
 		}
 	}
 	
-	public void getRelTile(int depth, int horizontal) {
-		
+	public Tile getRelTile(int parallelCoord, int perpendicularCoord){
+		int x = playerX + facing.getX() * parallelCoord + facing.getY() * perpendicularCoord;
+		int y = playerY + facing.getY() * parallelCoord + facing.getX() * perpendicularCoord;
+		Tile relTile = map[x][y];
+		return relTile;
 	}
+	
+	public Tile[][] getMap(){
+		return map;
+	}
+	
+	public void setMap(Tile [][]map){
+		this.map = map;
+	}
+	
+	public Tile getCurrentMapTile(){
+		return map[playerX][playerY];
+	}
+	
+	public void setLevel(int level){
+		this.level = level;
+	}
+	
+	public int getLevel(){
+		return this.level;
+	}
+	
 }
