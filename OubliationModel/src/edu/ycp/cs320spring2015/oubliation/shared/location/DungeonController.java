e23b72d2acee8bf5b42074853e5bb7be49d73562
Kevin Grasso <kevgrasso@gmail.com>
2015-05-03 23:54:49 -0400

    Split dungeon controller functions into DungeonController, finished implementing tile interactions

--- OubliationModel/src/edu/ycp/cs320spring2015/oubliation/shared/location/DungeonController.java
+++
@@ -1,8 +0,0 @@
-package edu.ycp.cs320spring2015.oubliation.shared.location;
-
-public interface DungeonController {
-	public void teleportAbsolute(int x, int y);
-	public void teleportRelative(int forward, int side);
-	
-	//TODO: profile proxys
-}
